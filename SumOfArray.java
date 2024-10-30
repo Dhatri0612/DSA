9138fa433e3790690c718bc7fb7bd64a4d41df7a
Dhatri <156882180+Dhatri0612@users.noreply.github.com>
2024-10-30 12:33:14 +0530

    Create SumOfArray.java

---
+++ SumOfArray.java
@@ -0,0 +1,12 @@
+package SumOfArray;
+
+public class main {
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        int sum=0;
+        for(int i =0;i<arr.length;i++){
+            sum+= arr[i];
+        }
+        System.out.println(sum);
+    }
+}
